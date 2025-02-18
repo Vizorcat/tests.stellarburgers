9d892b294c2b1e82012398dc7a9d11137805c415
Artem Pavlov <vizorcat@yandex.ru>
2025-02-18 15:05:05 +0300

    Добавил тестовые классы.

--- tests/test_autoregtest.py
+++ tests/test_autoregtest.py
@@ -1,10 +1,11 @@
 from locators.locators import Locators
 
-def test_login_with_generated_credentials(driver, credentials):
-    driver.get("https://stellarburgers.nomoreparties.site/login")
+class TestLogin:
+    def test_login_with_generated_credentials(driver, credentials):
+        driver.get("https://stellarburgers.nomoreparties.site/login")
 
-    driver.find_element(*Locators.LOGIN_EMAIL_FIELD).send_keys(credentials["email"])
-    driver.find_element(*Locators.LOGIN_PASSWORD_FIELD).send_keys(credentials["password"])
-    driver.find_element(*Locators.LOGIN_BUTTON).click()
+        driver.find_element(*Locators.LOGIN_EMAIL_FIELD).send_keys(credentials["email"])
+        driver.find_element(*Locators.LOGIN_PASSWORD_FIELD).send_keys(credentials["password"])
+        driver.find_element(*Locators.LOGIN_BUTTON).click()
 
-    assert "https://stellarburgers.nomoreparties.site/" in driver.current_url
+        assert "https://stellarburgers.nomoreparties.site/" in driver.current_url
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,23 +2,24 @@ import pytest
 from helpers.data_generators import generate_email, generate_password
 from locators.locators import Locators
 
-def test_successful_registration(driver):
-    driver.get("https://stellarburgers.nomoreparties.site/register")
+class TestRegistration:
+    def test_successful_registration(driver):
+        driver.get("https://stellarburgers.nomoreparties.site/register")
 
-    random_email = generate_email()
-    random_password = generate_password(8)
+        random_email = generate_email()
+        random_password = generate_password(8)
 
-    driver.find_element(*Locators.NAME_FIELD).send_keys("test1234")
-    driver.find_element(*Locators.EMAIL_FIELD).send_keys(random_email)
-    driver.find_element(*Locators.PASSWORD_FIELD).send_keys(random_password)
-    driver.find_element(*Locators.REGISTER_BUTTON).click()
+        driver.find_element(*Locators.NAME_FIELD).send_keys("test1234")
+        driver.find_element(*Locators.EMAIL_FIELD).send_keys(random_email)
+        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(random_password)
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
 
-def test_registration_invalid_password(driver):
-    driver.get("https://stellarburgers.nomoreparties.site/register")
-    driver.find_element(*Locators.NAME_FIELD).send_keys("ТестРег2")
-    driver.find_element(*Locators.EMAIL_FIELD).send_keys("testreg2@yandex.ru")
-    driver.find_element(*Locators.PASSWORD_FIELD).send_keys("123")
-    driver.find_element(*Locators.REGISTER_BUTTON).click()
+    def test_registration_invalid_password(driver):
+        driver.get("https://stellarburgers.nomoreparties.site/register")
+        driver.find_element(*Locators.NAME_FIELD).send_keys("ТестРег2")
+        driver.find_element(*Locators.EMAIL_FIELD).send_keys("testreg2@yandex.ru")
+        driver.find_element(*Locators.PASSWORD_FIELD).send_keys("123")
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
 
-    error_message = driver.find_element(*Locators.REGISTRATION_ERROR).text
-    assert "Некорректный пароль" in error_message
+        error_message = driver.find_element(*Locators.REGISTRATION_ERROR).text
+        assert "Некорректный пароль" in error_message
